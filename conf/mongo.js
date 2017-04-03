e5f66b273038d72a39fef7373521c3775bb378c8
Konstantin Lazutin <klazutin@gmail.com>
2017-04-02 20:47:46 -0600

    Preparation for Docker continues

--- conf/mongo.js
+++ conf/mongo.js
@@ -1,4 +1,8 @@
-use tvgraph
-db.createUser({user: 'tvgraph', pwd: 'tvgraph123', roles: ['readWrite']})
-db.tvseries.createIndex({'created_at': 1}, {expireAfterSeconds: 43200})
-db.tvseries.createIndex({show_title: 'text'})
+db = db.getSiblingDB("tvgraph");
+sleep(1000);
+db.createUser({user: "tvgraph", pwd: "tvgraph123", roles: ["readWrite"]});
+sleep(1000);
+db.tvseries.createIndex({show_title: "text"});
+sleep(1000);
+db.tvseries.createIndex({"created_at": 1}, {expireAfterSeconds: 43200});
+sleep(1000);
